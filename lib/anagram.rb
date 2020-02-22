f5b17e385759aa2265a7b6b52b0ec54de15138f8
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-22 17:35:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,8 @@ class Anagram
   end
   
   def match(array)
-    array.select do {|word| (word.split("").sort)}
+    array.select do |word| 
+      word.split("").sort == word.split("").sort
   end
 end
   
